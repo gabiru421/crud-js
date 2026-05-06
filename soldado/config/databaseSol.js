4a56717f30065a09077ffe3e1aa63a832c7b3b7a
gabiru421 <gwsantos18@gmail.com>
2026-05-06 14:12:01 -0300

    soldado

---
+++ soldado/config/databaseSol.js
@@ -0,0 +1,20 @@
+import { Sequelize } from 'sequalize';
+
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,   
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: "mysql",
+        logging: false,
+    });
+
+
+export const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Banco conectado com sucesso!");
+    } catch (error) {
+        console.error(`Erro ao conectar: ${error}`);
+    }
+
+};
