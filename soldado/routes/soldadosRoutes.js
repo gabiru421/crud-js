018d924489843822cbf4cebb3271be88ebfcbb17
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2026-05-06 22:00:24 -0300

    att

--- soldado/routes/soldadosRoutes.js
+++ soldado/routes/soldadosRoutes.js
@@ -11,6 +11,19 @@ import{
 const router = Router();
 
 router.post("/soldados", createSoldado);
+
+/**
+ * @swagger
+ * /soldados:
+ * get:
+ *      summary: Lista todos os soldados
+ *      tags: [Soldados]    
+ *      responses:          
+ *          200:
+ *              description: Lista de soldados
+ *          500:
+ *              description: Erro ao listar soldados 
+ */
 router.get("/soldados", getSoldados);
 router.get("/soldados/:id", getSoldado);
 router.put("/soldados/:id", updateSoldado);
